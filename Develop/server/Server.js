af0c6d9d0ed02874d86d1f0b1589e7b9f4d653b3
RickyCohen <ricky.cohen88@gmail.com>
2020-12-10 23:25:37 -0600

    fixed path to index.html

--- Develop/server/Server.js
+++ Develop/server/Server.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
 app.get("/",function(req,res){
-    res.sendFile(path.join(_dirname,"index.html"));
+    res.sendFile(path.join(_dirname,"../public/index.html"));
 });
 
 
