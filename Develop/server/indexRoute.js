bf64d4721590cedcbd65090d702aecb98697d626
RickyCohen <ricky.cohen88@gmail.com>
2020-12-11 15:51:30 -0600

    added routing to server files, work now

--- Develop/server/indexRoute.js
+++ Develop/server/indexRoute.js
@@ -1,3 +1,11 @@
 //Dependencies
 const path = require("path");
 
+module.exports = function(app){
+    app.get("/*"), function(req,res){
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    }
+
+
+
+};//end of export function
--- Develop/server/noteRoute.js
+++ Develop/server/noteRoute.js
@@ -2,3 +2,9 @@
 const path = require("path");
 
 
+module.exports = function (){
+    app.get("/notes"), function(req,res){
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    }
+};// end of exports function
+
