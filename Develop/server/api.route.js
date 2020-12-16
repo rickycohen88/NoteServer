c2451a9a100da7464876b51211287b5d157b25a8
RickyCohen <ricky.cohen88@gmail.com>
2020-12-16 10:12:37 -0600

    created a log file for all notes

--- Develop/server/api.route.js
+++ Develop/server/api.route.js
@@ -10,32 +10,41 @@ module.exports = function(app){
         //return res.sendFile(path.join__dirname,"..db/db.json");
     });
     app.post("/api/notes", function(req,res){
-    console.log("have recived post");
-    let notes = [];
-    let newNote = req.body;
+        console.log("have recived post");
+        let notes = [];
+        let newNote = req.body;
 
-    notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
+        notes = JSON.parse(fs.readFileSync(path.join(__dirname, "../db/db.json")));
 
-    newNote.id = id;
-    id++;
+        newNote.id = id;
+        id++;
 
-    notes.push(newNote);
-    notes = JSON.stringify(notes);
+        notes.push(newNote);
+        notes = JSON.stringify(notes);
 
-    fs.writeFile(path.join(__dirname, "../db/db.json"), notes, (err) =>
-    {
-        if(err)
+        fs.writeFile(path.join(__dirname, "../db/db.json"), notes, (err) =>
         {
-            return console.log(err);
-        }
-        else
+            if(err)
+            {
+                return console.log(err);
+            }
+            else
+            {
+                console.log("File Saved!");
+            }
+        });
+        fs.writeFile(path.join(__dirname, "../db/log.jsonc"), notes, (err) =>
         {
-            console.log("File Saved!");
-        }
-
-        res.end();
-    });
-
+            if(err)
+            {
+                return console.log(err);
+            }
+            else
+            {
+                console.log("File Loged!");
+            }
+        });
+    res.end();
     });
     app.delete("/api/notes/:id",function(req,res){
         let deleteID = req.params.id;
